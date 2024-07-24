42ca2297f5adc354d58777c54c7f37fff2082ca3
Geetikapandey5 <geetikapandey5082001@gmail.com>
2024-07-24 22:51:51 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://gleeful-ganache-d4e3cb.netlify.app'
+    'https://gleeful-ganache-d4e3cb.netlify.app',
+    'https://geetikapandeybloging.netlify.app/register'
 
 ];
 
